a0b650eb38e19fefcf0385c4a49de90e18d5be11
Jason A. Fager <jafager@jafager.com>
2026-06-04 15:54:50 -0400

    Added date and time to hello world message and updated test.

--- flaskhello/src/app.py
+++ flaskhello/src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello, world!</p>'
+    return '<p>Hello, world! The date and time is {}</p>'.format(datetime.now())
