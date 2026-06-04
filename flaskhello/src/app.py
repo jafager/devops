58f47f8e69f0b12e21dece97ff8282b84f453c69
Jason A. Fager <jafager@jafager.com>
2026-06-04 17:35:12 -0400

    Tweaked app page to trigger GHA.

--- flaskhello/src/app.py
+++ flaskhello/src/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello, world! The date and time is {}</p>'.format(datetime.now())
+    return '<p>Hello, world! The date and time is {}.</p>'.format(datetime.now())
